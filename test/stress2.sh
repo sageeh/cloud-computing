1801065f169402c9bc2eee255b05815ec985a7c6
sageeh <hamati.best1@gmail.com>
2026-04-08 15:26:41 +0200

    removed tmp folder and added stress test 2

--- test/stress2.sh
+++ test/stress2.sh
@@ -1,7 +1,7 @@
 for i in {1..5000}; do 
   curl -s -X POST http://localhost:8080/orders \
     -H "Content-Type: application/json" \
-    -d "{\"id\": \"order-$RANDOM-$i\", \"item\": \"Titanium Keyboard\", \"quantity\": 1}" > /dev/null & 
+    -d "{\"id\": \"order-$RANDOM-$i\", \"item\": \"Shampoo\", \"quantity\": 1}" > /dev/null & 
 done
 
 wait
